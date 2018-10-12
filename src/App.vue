5def6847df07a039764759d4cf0534e529713b6d
shadddy <437571345@qq.com>
2018-10-12 17:45:49 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="header">
+        头部
+    </div>
+    <div class="tab">
+      导航
+    </div>
+    <div class="content">
+      内容
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+
+</style>
